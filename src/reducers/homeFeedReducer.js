06457957a1f8b8467a538c6d55169679df151f44
Naman-Saxena1 <namansaxena.official@gmail.com>
2022-04-15 20:23:22 +0530

    feat: Show latest post on top of home feed

--- src/reducers/homeFeedReducer.js
+++ src/reducers/homeFeedReducer.js
@@ -1,7 +1,7 @@
 const homeFeedReducer = (state = [], { type, payload}) => {
     switch(type)
     {
-        case "UPDATE_HOME_FEED" :  return [...payload]
+        case "UPDATE_HOME_FEED" :  return [...(payload.reverse()) ]
         default : return [...state]
     }
 }
