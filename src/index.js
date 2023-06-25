289bec2f5eaa597230dcbd2f510b2281916b9a17
Naman-Saxena1 <namansaxena.official@gmail.com>
2023-06-25 22:36:48 +0530

    fix: Minor fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import App from './App';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import store from "./store"
